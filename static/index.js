358e665c422c5aaf9c4a7229a19938fa3800700f
Liuhua-six <116743735+Liuhua-six@users.noreply.github.com>
2022-10-27 02:11:05 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://liuhua-six.github.io/main/static/image/ClickBefore.png';
+var url = 'https://upload.cc/i1/2022/10/27/z3KHeZ.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
